921c7402568c16b5e65a11f325bca766940047c8
norestraint <jorge.luis@nubank.com.br>
2021-07-28 00:31:19 -0300

    Create client and credit card(with timestamp and random number).

--- src/desafio/core.clj
+++ src/desafio/core.clj
@@ -1,6 +1,36 @@
 (ns desafio.core)
+; Necessario pra usar a biblioteca de tempo, tive que fazer uma mega "gambiarra" pra nao dar conflito entre funcoes do pacote e do clojure.
+(use '[java-time :exclude [range iterate format max min contains? zero?]])
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; Roubei daqui https://stackoverflow.com/questions/27053726/how-to-generate-random-password-with-the-fixed-length-in-clojure
+(defn gerar-numero-cartao
+  ([] (gerar-numero-cartao 10))
+  ([n]
+   (let [chars-between #(map char (range (int %1) (inc (int %2))))
+         chars (concat (chars-between \0 \9))
+         numero (take n (repeatedly #(rand-nth chars)))]
+     (reduce str numero))))
+
+(defn criar-cartao
+  "Cria um hashmap com as informações do cartão do cliente."
+  [limite]
+  {
+   :numero   (gerar-numero-cartao)
+   :limite   limite
+   :validade (plus (local-date) (years 8))                  ; Nao sei porque ele nao reconhece as funcoes mas tá funcionando.
+   }
+  )
+
+(defn criar-cliente
+  "Cria um hashmap com os dados do cliente."
+  [nome cpf email limite]
+  {
+   :nome   nome
+   :cpf    cpf
+   :email  email
+   :cartao (criar-cartao limite)
+   }
+  )
+
+;(def cliente (criar-cliente "Jorge Luis" 9233 "jorge@email" 1000))
+;(println cliente)
